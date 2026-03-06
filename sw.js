a1e061ebc015ba5d21303764d7bd1cc6170f51fa
31nomekop <31nomekop@gmail.com>
2026-03-06 20:40:05 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,32 +1,31 @@
-const CACHE_NAME = 'echoes-of-the-lantern-v0-1';
-const ASSETS = [
-  './',
-  './index.html',
-  './styles.css',
-  './app.js',
-  './manifest.json',
-  './sw.js',
-  './assets/icons/icon-192.png',
-  './assets/icons/icon-512.png',
-  './assets/icons/icon-1024.png',
-  './assets/icons/apple-touch-icon.png',
-  './assets/icons/favicon-32.png'
+const CACHE_NAME = "echoes-of-the-lantern-v0.2-cache";
+const FILES = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png",
+  "./title-bg.png",
+  "./journal-bg.png"
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)).then(()=>self.skipWaiting())
   );
-  self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    caches.match(event.request).then(res => res || fetch(event.request).catch(()=>caches.match("./index.html")))
   );
 });
